e7131a11fd3bdcbff41a83b1cb65c19d87bd70f8
JoyRume <joy.rume@student.moringaschool.com>
2023-12-24 15:20:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+//function superbowlWin(){
+  //  return result.find(record=> record.result==="W")
+//}
+
+function superbowlWin(record){
+    const result = record.find( record => record.result === "W" );
+    return result? result.year : undefined;
+ }
